5b359a815cbd782a33b60d5b58254b752b5ff402
ercr07001 <ercr07001@E-PW05CGR0>
2024-12-18 12:09:46 +0100

    Add project files.

---
+++ Fraga_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Fraga_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String password;
+
+            do
+            {
+                Console.WriteLine("Ange lösenordet: ");
+                Console.WriteLine("Hint: Lösenordet är Hemligt");
+                password = Console.ReadLine();
+            }   while (password != "Hemligt");
+
+
+
+        }
+    }
+}
